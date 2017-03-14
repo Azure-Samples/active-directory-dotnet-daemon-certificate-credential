72fdba9d09c032ea070a28cec55295aeb23a5cf9
Jean-Marc Prieur <jmprieur@microsoft.com>
2017-03-14 22:12:04 +0100

    Adding TenantId and Credential parameters to the script.

--- Configure.ps1
+++ Configure.ps1
@@ -23,9 +23,9 @@
 # 6) Optionnaly if you want to cleanup the applications in the Azure AD, run:
 #      CleanUp $apps
 #    The applications are un-registered
+param([PSCredential]$Credential, [string]$TenantId)
 Import-Module AzureAD
 $ErrorActionPreference = 'Stop'
-
 
 # Replace the value of an appsettings of a given key in an XML App.Config file.
 Function ReplaceSetting([string] $configFilePath, [string] $key, [string] $newValue)
@@ -76,6 +76,7 @@ so that they are consistent with the Applications parameters
     [CmdletBinding()]
     param(
         [Parameter(Mandatory=$True, HelpMessage='Tenant ID (This is a GUID which represents the "Directory ID" of the AzureAD tenant into which you want to create the apps')]
+        [PSCredential] $Credential,
         [string] $tenantId
     )
 
@@ -84,8 +85,24 @@ so that they are consistent with the Applications parameters
     # $tenantId is the Active Directory Tenant. This is a GUID which represents the "Directory ID" of the AzureAD tenant 
     # into which you want to create the apps. Look it up in the Azure portal in the "Properties" of the Azure AD. 
 
-    # Login to Azure PowerShell (interactive: you'll need to sign-in with creds enabling your to create apps in the tenant)
-    $creds = Connect-AzureAD -TenantId $tenantId
+    # Login to Azure PowerShell (interactive if credentials are not already provided: 
+    # you'll need to sign-in with creds enabling your to create apps in the tenant)
+    if (!$Credential)
+    {
+        $creds = Connect-AzureAD -TenantId $tenantId
+    }
+    else
+    {
+        if (!$TenantId)
+        {
+            $creds = Connect-AzureAD -Credential $Credential
+        }
+        else
+        {
+            $creds = Connect-AzureAD -TenantId $tenantId -Credential $Credential
+        }
+    }
+
     if (!$tenantId)
     {
         $tenantId = $creds.Tenant.Id
@@ -171,9 +188,9 @@ Function CleanUp($applicationsInformation)
 
 
 # Run interactively (will ask you for the tenant ID)
-$apps = ConfigureApplications 
+$apps = ConfigureApplications -Credential $Credential -tenantId $TenantId
 
-# you can also provide the tenant ID
+# you can also provide the tenant ID and the credentials
 # $tenantId = "ID of your AAD directory"
 # $apps = ConfigureApplications -tenantId $tenantId 
 
