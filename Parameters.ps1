db21ff10f17b99458b869eb4b9ee9dec01cb93d6
Jean-Marc Prieur <jmprieur@microsoft.com>
2017-03-16 16:54:15 +0100

    Improving the parameters as suggested by Matthew.

--- Parameters.ps1
+++ Parameters.ps1
@@ -5,6 +5,6 @@
 
     # Variables for the registration of the AAD application for the Daemon app
     $daemonAadAppName = "TodoListDaemonWithCert"
-    $daemonHomePage = "https://TodoListDaemonWithCert"
-    $daemonAppIdURI = "https://TodoListDaemonWithCert"
+    $daemonHomePage = "https://$daemonAadAppName"
+    $daemonAppIdURI = "https://$daemonAadAppName"
     $certificateName = "CN=TodoListDaemonWithCert"
