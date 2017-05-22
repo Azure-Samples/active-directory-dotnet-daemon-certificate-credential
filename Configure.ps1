b0db5f715fff0aacb4ff9fad03139d0234cb9ef7
Navya Canumalla <nacanuma@microsoft.com>
2017-05-21 23:02:01 -0700

    Improve README instructions. (#34)

--- Configure.ps1
+++ Configure.ps1
@@ -1,17 +1,18 @@
 # This script creates the Azure AD applications needed for this sample and updates the configuration files
 # for the visual Studio projects from the data in the Azure AD applications.
 #
-# Before running this script you need to install the AzureAD cmdlets as an administrator. 
+# Before running this script you need to install the AzureAD cmdlets as an administrator.
 # For this:
 # 1) Run Powershell as an administrator
 # 2) in the PowerShell window, type: Install-Module AzureAD
+# 3) Set the ExecutionPolicy to Unrestricted: Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope Process
 #
-# Before you run this script
-# 3) With the Azure portal (https://portal.azure.com), choose your active directory tenant, then go to the Properties of the tenant and copy
+# To run this script
+# 4) With the Azure portal (https://portal.azure.com), choose your active directory tenant, then go to the Properties of the tenant and copy
 #    the DirectoryID. This is what we'll use in this script for the tenant ID
-# 
-# To configurate the applications
-# 4) Run the following command:
+#
+# To configure the applications
+# 5) Run the following command in the project root:
 #      $apps = ConfigureApplications -tenantId [place here the GUID representing the tenant ID]
 #    You will be prompted by credentials, be sure to enter credentials for a user who can create applications
 #    in the tenant
@@ -20,7 +21,7 @@
 # 5) Build and execute the applications. This just works
 #
 # To cleanup
-# 6) Optionnaly if you want to cleanup the applications in the Azure AD, run:
+# 6) Optionaly if you want to cleanup the applications in the Azure AD, run:
 #      CleanUp $apps
 #    The applications are un-registered
 param([PSCredential]$Credential="", [string]$TenantId="")
@@ -31,7 +32,7 @@ $ErrorActionPreference = 'Stop'
 Function ReplaceSetting([string] $configFilePath, [string] $key, [string] $newValue)
 {
     [xml] $content = Get-Content $configFilePath
-    $appSettings = $content.configuration.appSettings; 
+    $appSettings = $content.configuration.appSettings;
     $keyValuePair = $appSettings.SelectSingleNode("descendant::add[@key='$key']")
     if ($keyValuePair)
     {
@@ -82,10 +83,10 @@ so that they are consistent with the Applications parameters
 
    process
    {
-    # $tenantId is the Active Directory Tenant. This is a GUID which represents the "Directory ID" of the AzureAD tenant 
-    # into which you want to create the apps. Look it up in the Azure portal in the "Properties" of the Azure AD. 
+    # $tenantId is the Active Directory Tenant. This is a GUID which represents the "Directory ID" of the AzureAD tenant
+    # into which you want to create the apps. Look it up in the Azure portal in the "Properties" of the Azure AD.
 
-    # Login to Azure PowerShell (interactive if credentials are not already provided: 
+    # Login to Azure PowerShell (interactive if credentials are not already provided:
     # you'll need to sign-in with creds enabling your to create apps in the tenant)
     if (!$Credential)
     {
@@ -130,11 +131,11 @@ so that they are consistent with the Applications parameters
     $certificate=New-SelfSignedCertificate -Subject $certificateName `
                                            -CertStoreLocation "Cert:\CurrentUser\My" `
                                            -KeyExportPolicy Exportable `
-                                           -KeySpec Signature 
+                                           -KeySpec Signature
     $certKeyId = [Guid]::NewGuid()
     $certBase64Value = [System.Convert]::ToBase64String($certificate.GetRawCertData())
     $certBase64Thumbprint = [System.Convert]::ToBase64String($certificate.GetCertHash())
-    
+
     # Add a Azure Key Credentials from the certificate for the daemon application
     $applicationKeyCredentials = New-AzureADApplicationKeyCredential -ObjectId $daemonApplication.ObjectId `
                                                                      -CustomKeyIdentifier $certificateName `
@@ -171,9 +172,9 @@ ConfigureApplications -Credential $Credential -tenantId $TenantId
 
 # you can also provide the tenant ID and the credentials
 # $tenantId = "ID of your AAD directory"
-# $apps = ConfigureApplications -tenantId $tenantId 
+# $apps = ConfigureApplications -tenantId $tenantId
 
 
-# When you have built your Visual Studio solution and ran the code, if you want to clean up the Azure AD applications, just 
+# When you have built your Visual Studio solution and ran the code, if you want to clean up the Azure AD applications, just
 # run the following command in the same PowerShell window as you ran ConfigureApplications
 # . .\CleanUp -Credentials $Credentials
